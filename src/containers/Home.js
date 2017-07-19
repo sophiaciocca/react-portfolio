7a9d49cf7ed0bd9bf58c75279e49aa0a6cc53171
Sophia Ciocca <sophiaciocca@gmail.com>
2017-07-19 17:14:39 -0400

    Create functioning react-router with dummy homepage

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is home!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import routes from './routes';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();
