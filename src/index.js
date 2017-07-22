3d3b832b443e9104e0ae72dcdce3320578e9190f
Sophia Ciocca <sophiaciocca@gmail.com>
2017-07-22 01:07:06 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import routes from './routes';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(routes, document.getElementById('root'));
-registerServiceWorker();
