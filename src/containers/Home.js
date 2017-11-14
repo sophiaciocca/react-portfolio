4c76fe96ad5b21174a03a05f51d4d1d5b361e7ee
Sophia Ciocca <sophiaciocca@gmail.com>
2017-11-13 23:30:56 -0500

    Update bio in Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
           ​Outside of code, I like to create in other ways. From <a href="https://medium.com/@sophiaciocca/latest" target="_blank">blogging</a> to <a href="https://www.youtube.com/channel/UChfrVcFHC-mfoqrJHuOYt-Q" target="_blank">music-making</a>, <a href="http://sophiacioccaphotography.tumblr.com/" target="_blank">photography</a> to <a href="https://www.instagram.com/sidewalk_poetry/" target="_blank">guerilla sidewalk chalk street art</a>, I'm always trying new ways of expressing myself. I also like to meditate, talk to strangers, and run lifestyle experiments.
         </p>
         <p>
-          I currently work as a teaching fellow at <a href="https://www.fullstackacademy.com" target="_blank">Fullstack Academy</a>, and am seeking a new full-time opportunity as I come upon the end of my fellowship. Before becoming a software developer, I served in the Peace Corps, and before that, I worked for IBM, management consulting for Fortune 100 companies. I also hold a degree in economics from the University of Pennsylvania.
+          I currently work as a software engineer at the New York Times. Previously, I worked as a teaching fellow at the <a href="https://www.gracehopper.com" target="_blank">Grace Hopper Program</a>. Before learning to develop software, I served in the Peace Corps and management consulted for Fortune 100 companies. I hold a degree in economics from the University of Pennsylvania.
         </p>
       </div>
     );
