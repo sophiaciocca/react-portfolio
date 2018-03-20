94baf36c1e5f9de831e4c654564b2f53ba5978ef
Sophia Ciocca <sophia.ciocca@nytimes.com>
2018-03-20 13:50:43 -0400

    Patch up code title CSS

--- src/containers/Code.js
+++ src/containers/Code.js
@@ -17,33 +17,34 @@ const CodeThumbnail = styled.img`
   margin: 0 auto;
 `;
 
+const PortAllText = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
 const PortTitleContainer = styled.div`
   position: relative;
-  width: 96%;
   margin: 0 auto;
-  -ms-flex-align: baseline;
   align-items: baseline;
-  display: -webkit-flex;
-  -webkit-flex-wrap: nowrap;
-  -webkit-justify-content: flex-end;
-  -webkit-align-content: stretch;
-  display: -ms-flexbox;
   display: flex;
-  -ms-flex-wrap: nowrap;
   flex-wrap: nowrap;
-  -ms-flex-direction: row;
   flex-direction: row;
-  -ms-flex-pack: end;
   justify-content: flex-end;
-  -ms-flex-line-pack: stretch;
   align-content: stretch;
+
+  @media (max-width: 600px) {
+    display: inline;
+  }
 `;
 
 const PortTitle = styled.h2`
   flex: 1;
-  margin-bottom: 20px;
   font-family: Gotham, Helvetica, Arial, sans-serif;
   text-transform: uppercase;
+
+  @media (max-width: 600px) {
+    text-align: center;
+  }
 `;
 
 export const PortButton = styled.a`
@@ -62,19 +63,30 @@ export const PortButton = styled.a`
     color: #fff;
     border-color: ${ColorScheme.primary};
   }
+
+  @media (max-width: 600px) {
+    padding: 5px 7px;
+  }
+`;
+
+const PortButtonContainer = styled.div`
+  width: 30%;
+  margin: 0 auto;
+
+  @media (max-width: 600px) {
+    margin-top: 15px;
+    width: 50%;
+  }
 `;
 
 const PortDescription = styled.p`
   display: block;
-  width: 96%;
-  margin: 0 auto;
-  margin-bottom: 25px;
+  margin: 15px 0;
 `;
 
 const UsedList = styled.ul`
   list-style: none;
   padding: 0;
-  width: 96%;
   margin: 0 auto;
 `;
 
@@ -141,40 +153,42 @@ class Code extends Component {
             <a href="https://align.fun/" target="_blank">
               <CodeThumbnail src={require("../assets/AlignScreenshot.png")} />
             </a>
-            <PortTitleContainer>
-              <PortTitle>Align</PortTitle>
-              <div>
-                <PortButton href="https://align.fun" target="_blank">
-                  Demo
+            <PortAllText>
+              <PortTitleContainer>
+                <PortTitle>Align</PortTitle>
+                <PortButtonContainer>
+                  <PortButton href="https://align.fun" target="_blank">
+                    Demo
                 </PortButton>
-                <PortButton
-                  href="https://github.com/align-capstone/align"
-                  target="_blank"
-                >
-                  Code
+                  <PortButton
+                    href="https://github.com/align-capstone/align"
+                    target="_blank"
+                  >
+                    Code
                 </PortButton>
-              </div>
-            </PortTitleContainer>
-            <PortDescription>
-              My capstone project from the Grace Hopper Program, Align is a web
-              application for setting and managing long-term goals with a
-              beautiful and intuitive interface. It includes image uploading
-              capability, resource cards to save helpful links, a rich text
-              editor, and live updating using Firebase WebSocket integration.
+                </PortButtonContainer>
+              </PortTitleContainer>
+              <PortDescription>
+                My capstone project from the Grace Hopper Program, Align is a web
+                application for setting and managing long-term goals with a
+                beautiful and intuitive interface. It includes image uploading
+                capability, resource cards to save helpful links, a rich text
+                editor, and live updating using Firebase WebSocket integration.
             </PortDescription>
-            <PortDescription>
-              I worked with two incredibly smart teammates to create Align in
+              <PortDescription>
+                I worked with two incredibly smart teammates to create Align in
               three weeks. You can read about our process building it{" "}
-              <a href="https://medium.com/ladies-storm-hackathons/how-we-built-our-first-full-stack-javascript-web-app-in-three-weeks-8a4668dbd67c">
-                here
+                <a href="https://medium.com/ladies-storm-hackathons/how-we-built-our-first-full-stack-javascript-web-app-in-three-weeks-8a4668dbd67c">
+                  here
               </a>.
             </PortDescription>
-            <UsedList>
-              <UsedItem>React.js</UsedItem>
-              <UsedItem>Firebase</UsedItem>
-              <UsedItem>Material-UI</UsedItem>
-              <UsedItem>Victory.js</UsedItem>
-            </UsedList>
+              <UsedList>
+                <UsedItem>React.js</UsedItem>
+                <UsedItem>Firebase</UsedItem>
+                <UsedItem>Material-UI</UsedItem>
+                <UsedItem>Victory.js</UsedItem>
+              </UsedList>
+            </PortAllText>
           </PortRow>
           <hr />
           <PortRow>
@@ -183,50 +197,52 @@ class Code extends Component {
                 src={require("../assets/OkCollabScreenshot.png")}
               />
             </a>
-            <PortTitleContainer>
-              <PortTitle>OkCollab</PortTitle>
-              <div>
-                <PortButton
-                  href="https://okcollab.herokuapp.com/"
-                  target="_blank"
-                >
-                  Demo
+            <PortAllText>
+              <PortTitleContainer>
+                <PortTitle>OkCollab</PortTitle>
+                <PortButtonContainer>
+                  <PortButton
+                    href="https://okcollab.herokuapp.com/"
+                    target="_blank"
+                  >
+                    Demo
                 </PortButton>
-                <PortButton
-                  href="https://github.com/sophiaciocca/ok-collab"
-                  target="_blank"
-                >
-                  Code
-                </PortButton>
-              </div>
-            </PortTitleContainer>
-            <PortDescription>
-              For my hackathon project at Grace Hopper, I spent three days
-              building an app I always wished I'd had -- a social network for
-              musicians to find other musicians to collaborate with. It's like
-              OkCupid, except instead of dates it's for finding people to jam
-              with and make tracks. View all users, view each user's profile and
-              information, play their embedded Youtube videos, and see similar
-              users in the sidebar.
+                  <PortButton
+                    href="https://github.com/sophiaciocca/ok-collab"
+                    target="_blank"
+                  >
+                    Code
+                  </PortButton>
+                </PortButtonContainer>
+              </PortTitleContainer>
+              <PortDescription>
+                For my hackathon project at Grace Hopper, I spent three days
+                building an app I always wished I'd had -- a social network for
+                musicians to find other musicians to collaborate with. It's like
+                OkCupid, except instead of dates it's for finding people to jam
+                with and make tracks. View all users, view each user's profile and
+                information, play their embedded Youtube videos, and see similar
+                users in the sidebar.
             </PortDescription>
-            <PortDescription>
-              OkCollab is responsive and works on devices of all sizes. You can
+              <PortDescription>
+                OkCollab is responsive and works on devices of all sizes. You can
               watch me present it live{" "}
-              <a
-                href="https://www.youtube.com/watch?v=qyLoInHNjoc"
-                target="_blank"
-              >
-                here
+                <a
+                  href="https://www.youtube.com/watch?v=qyLoInHNjoc"
+                  target="_blank"
+                >
+                  here
               </a>.
             </PortDescription>
-            <UsedList>
-              <UsedItem>React.js</UsedItem>
-              <UsedItem>Redux</UsedItem>
-              <UsedItem>PostgreSQL</UsedItem>
-              <UsedItem>CSS</UsedItem>
-              <UsedItem>Node.JS</UsedItem>
-              <UsedItem>Express</UsedItem>
-            </UsedList>
+              <UsedList>
+                <UsedItem>React.js</UsedItem>
+                <UsedItem>Redux</UsedItem>
+                <UsedItem>PostgreSQL</UsedItem>
+                <UsedItem>CSS</UsedItem>
+                <UsedItem>Node.JS</UsedItem>
+                <UsedItem>Express</UsedItem>
+              </UsedList>
+            </PortAllText>
           </PortRow>
           <hr />
           <PortRow>
@@ -235,37 +251,39 @@ class Code extends Component {
                 src={require("../assets/LimitlessLeggingsScreenshot.png")}
               />
             </a>
-            <PortTitleContainer>
-              <PortTitle>Limitless Leggings</PortTitle>
-              <div>
-                <PortButton
-                  href="https://limitless-leggings.herokuapp.com/"
-                  target="_blank"
-                >
-                  Demo
+            <PortAllText>
+              <PortTitleContainer>
+                <PortTitle>Limitless Leggings</PortTitle>
+                <PortButtonContainer>
+                  <PortButton
+                    href="https://limitless-leggings.herokuapp.com/"
+                    target="_blank"
+                  >
+                    Demo
                 </PortButton>
-                <PortButton
-                  href="https://github.com/limitless-leggings/limitless-leggings"
-                  target="_blank"
-                >
-                  Code
-                </PortButton>
-              </div>
-            </PortTitleContainer>
-            <PortDescription>
-              Limitless Leggings is a full-stack e-commerce site built from
-              scratch. It includes products filterable by category; working
-              signup, login, and o-auth capabilities; and a working cart model
-              built in PostgreSQL.
+                  <PortButton
+                    href="https://github.com/limitless-leggings/limitless-leggings"
+                    target="_blank"
+                  >
+                    Code
+                  </PortButton>
+                </PortButtonContainer>
+              </PortTitleContainer>
+              <PortDescription>
+                Limitless Leggings is a full-stack e-commerce site built from
+                scratch. It includes products filterable by category; working
+                signup, login, and o-auth capabilities; and a working cart model
+                built in PostgreSQL.
             </PortDescription>
-            <UsedList>
-              <UsedItem>React.js</UsedItem>
-              <UsedItem>Redux</UsedItem>
-              <UsedItem>PostgreSQL</UsedItem>
-              <UsedItem>CSS</UsedItem>
-              <UsedItem>Node.JS</UsedItem>
-              <UsedItem>Express</UsedItem>
-            </UsedList>
+              <UsedList>
+                <UsedItem>React.js</UsedItem>
+                <UsedItem>Redux</UsedItem>
+                <UsedItem>PostgreSQL</UsedItem>
+                <UsedItem>CSS</UsedItem>
+                <UsedItem>Node.JS</UsedItem>
+                <UsedItem>Express</UsedItem>
+              </UsedList>
+            </PortAllText>
           </PortRow>
         </div>
       </div>
