47380247aa88687dcb7144e3fd52c7f2204a1b8c
Sophia Ciocca <sophia.ciocca@nytimes.com>
2019-10-29 17:19:07 -0400

    change copy a bit

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -47,7 +47,7 @@ class Footer extends Component {
             </div>
             <div className="row" id="copyright" style={{ margin: 0 }}>
               <div className="col-lg-12">
-                <p className="small">© 2017 Sophia Ciocca</p>
+                <p className="small">© 2019 Sophia Ciocca</p>
               </div>
             </div>
       </FooterDiv>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
       <div>
         <CenteredHeader>Hi! I'm Sophia. I'm a software developer based in NYC.</CenteredHeader>
         <p>
-          I'm comfortable throughout the stack, but my passion lies in the front end, at the intersection of code and design. I currently build software for the newsroom at the New York Times.
+          I've worked throughout the stack, but I especially love building React UI. I currently build software for the newsroom at the New York Times.
         </p>
         <p>
           ​Outside of code, I like to create in other ways. From <a href="https://medium.com/@sophiaciocca/latest" target="_blank">blogging</a> to <a href="https://www.youtube.com/channel/UChfrVcFHC-mfoqrJHuOYt-Q" target="_blank">music-making</a>, <a href="http://sophiacioccaphotography.tumblr.com/" target="_blank">photography</a> to <a href="https://www.instagram.com/sidewalk_poetry/" target="_blank">guerilla sidewalk chalk street art</a>, I'm always trying new ways of expressing myself. I also like to meditate, talk to strangers, and run lifestyle experiments.
